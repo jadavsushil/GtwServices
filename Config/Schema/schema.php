360403bc65af6a1e6c9beff11f0be095016e0cf2
jadavsushil <jadavsushil@gmail.com>
2014-12-03 18:40:22 +0530

    - Added Schema File

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -8,6 +8,22 @@ class GtwServiceSchema extends CakeSchema {
 	public function after($event = array()) {
 	}
 
+	public $service_categories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'description' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'status' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 5),
+		'price' => array('type' => 'float', 'null' => false, 'default' => null, 'length' => '12,3'),
+		'is_custom' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 2),
+		'service_count' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
 	public $service_files = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
 		'service_id' => array('type' => 'integer', 'null' => false, 'default' => null),
@@ -21,6 +37,7 @@ class GtwServiceSchema extends CakeSchema {
 	public $services = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
 		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'service_category_id' => array('type' => 'integer', 'null' => false, 'default' => '1'),
 		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'description' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'price' => array('type' => 'float', 'null' => false, 'default' => null, 'length' => '12,3'),
